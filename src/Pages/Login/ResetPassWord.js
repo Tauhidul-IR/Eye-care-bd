062000edbd007638ca713cb5d058f4d16269b114
Tauhidul Islam Robin <robin13690@gmail.com>
2022-12-26 10:18:54 +0600

    send password reset email

--- src/Pages/Login/ResetPassWord.js
+++ src/Pages/Login/ResetPassWord.js
@@ -8,16 +8,19 @@ const ResetPassWord = () => {
 
     //Onblur to get email
     const handleEmail = event => {
-        event.preventDefault()
         const email = event.target.value;
+        console.log(email);
         setUserEmail(email)
     }
 
     //Reset password function
-    const handleReset = () => {
+    const handleReset = (event) => {
+        event.preventDefault()
+        const form = event.target
         resetPassword(userEmail)
             .then(() => {
-                toast.success("PassWord Email send.")
+                alert("Reset PassWord Email send.")
+                form.reset()
             })
             .catch(error => console.log(error))
     }
@@ -26,11 +29,11 @@ const ResetPassWord = () => {
 
     return (
         <div>
-            <form className='h-96 text-center pt-24'>
+            <form onSubmit={handleReset} className='h-96 text-center pt-24'>
                 <h2 className='text-3xl mb-7 font-bold'>Reset Password</h2>
                 <input onBlur={handleEmail} name='email' type="email" placeholder="Email" className="input input-bordered w-full max-w-xs" />
                 <br />
-                <input onClick={handleReset} type="submit" className='btn btn-info mt-5' value={'Reset password'} />
+                <input type="submit" className='btn btn-info mt-5' value={'Reset password'} />
             </form>
         </div>
     );
